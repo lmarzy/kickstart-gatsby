01f7823aa951c0335cbea0d1e87ea571dfbbfd5d
lmarzy <me@phz3.net>
2019-08-15 20:36:46 +0100

    Updated home banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -5,7 +5,11 @@ import './banner.scss';
 import bannerImage from '@assets/images/banner.jpg';
 
 export const Banner = () => (
-  <aside className="c-banners">
-    <img src={bannerImage} alt="sup" className="c-banners__img" />
+  <aside className="c-banner">
+    {/* <img src={bannerImage} alt="sup" className="c-banners__img" /> */}
+    <div>
+      <h2 className="c-banner__title">Personalised</h2>
+      <p className="c-banner__info">Whiteboards, Notepads, Signs and Gifts</p>
+    </div>
   </aside>
 );
